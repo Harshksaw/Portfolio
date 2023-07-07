c43af56102aa8b98eb7c04bb450e9df6bf46741a
Harsh <mister.harshkumar@gmail.com>
2023-07-07 21:27:44 +0530

    Added clock , style changes

--- protfolio.js
+++ protfolio.js
@@ -21,3 +21,34 @@ document.querySelector('.switch').addEventListener("click", ()=>{
        
     }
 })
+function getDateTime() {
+    var now = new Date();
+    var year = now.getFullYear();
+    var month = now.getMonth() + 1;
+    var day = now.getDate();
+    var hour = now.getHours();
+    var minute = now.getMinutes();
+    var second = now.getSeconds();
+    if (month.toString().length == 1) {
+            month = '0' + month;
+    }
+    if (day.toString().length == 1) {
+            day = '0' + day;
+    }
+    if (hour.toString().length == 1) {
+            hour = '0' + hour;
+    }
+    if (minute.toString().length == 1) {
+            minute = '0' + minute;
+    }
+    if (second.toString().length == 1) {
+            second = '0' + second;
+    }
+    var dateTime = year + '/' + month + '/' + day + ' ' + hour + ':' + minute + ':' + second;
+    return dateTime;
+}
+setInterval(()=>{
+    let d = getDateTime()
+    let c = document.getElementById('clock')
+    c.innerHTML = d
+})
