e7e281aff95b3e826a6c9aafa348d48a838b30d2
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2023-11-19 17:56:04 +0530

    React Portfolio

---
+++ App.jsx
@@ -0,0 +1,18 @@
+
+
+import Home from './Components/Navbar/Home';
+import Navbar from './Components/Navbar/NavBar';
+
+function App() {
+
+
+  return (
+    <>
+    <Home/>
+    {/* <Navbar/> */}
+    </>
+   
+  )
+}
+
+export default App
