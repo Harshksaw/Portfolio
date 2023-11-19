579cb49457d77af27c97273a1f1e94e0b79e1166
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2023-11-19 14:13:03 +0000

    SocialLinks

--- src/Components/SocialLinks.jsx
+++ src/Components/SocialLinks.jsx
@@ -1,25 +1,75 @@
-import { FaGithub , FaLinkedin } from "react-icons/fa"
-import {HIOutlineMail} from "react-icons/hi"
-import {BsFillPersonLinesFill} from "react-icons/bs"
+import { FaGithub, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
+// import { HIOutlineMail } from "react-icons/hi";
+// import { BsFillPersonLinesFill } from "react-icons/bs";
 
+const SocialLinks = () => {
+  const links = [
+    {
+      id: 1,
+      child: (
+        <>
+          Linkedin <FaLinkedin size={30} />
+        </>
+      ),
+      href: "https://www.linkedin.com/feed/",
+      style: "rounded-br-md",
+    },
+    {
+      id: 2,
+      child: (
+        <>
+          Github <FaGithub size={30} />{" "}
+        </>
+      ),
+      href: "https://github.com/Harshksaw",
+      style: "rounded-br-md",
+    },
+    {
+      id: 3,
+      child: (
+        <>
+          Instagram <FaInstagram size={30} />{" "}
+        </>
+      ),
+      href: "https://www.linkedin.com/feed/",
+      style: "rounded-br-md",
+    },
+    {
+      id: 4,
+      child: (
+        <>
+          Twitter <FaTwitter size={30} />{" "}
+        </>
+      ),
+      href: "https://www.linkedin.com/feed/",
+      style: "rounded-br-md",
+    },
+  ];
 
-
-export default function SocialLinks() {
   return (
-    <div className="flex flex-col top-[35%] left-0 fixed">
+    <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
 
-            <ul>
-                <li className="flex justify-between items-center w-40 h-14 px-4 bg-gray-500">
-                    <a href= "" className="flex justify-between items-center w-full text-white">
-                        <>
-                            Linkedin <FaLinkedin size={30}/>
-                        </>
 
-                    </a>
-                </li>
+      <ul>
+        {links.map(({child , id , href , style })=>{
+          return (
+            <li key={id} 
+            className={"flex justify-between items-center w-40 h-14 px-4 ml-[-100px]  hover:ml-[10px] hover:rounded-md    duration-300 bg-gray-600" + " " + style}
 
-            </ul>
-      
-    </div>
+            >
+              <a href={href} className="flex justify-between items-center w-full text-blue-400" target="_blank" rel="noreferrer" >
+                {child}
+              </a>
+  
+            </li>
+          )
+          
+        })}
+       
+
+
+      </ul >
+    </div >
   )
-}
+};
+export default SocialLinks;
