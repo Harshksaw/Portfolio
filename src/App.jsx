0d7c4f68d0774fcb93c807460e13a35e875a71b1
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2023-11-19 20:25:47 +0530

    React Portfolio structure

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+// import SocialLinks from './src/Components/SocialLinks';
+import Navbar from './Components/NavBar';
+import Home from './Components/Home';
+import SocialLinks from './Components/SocialLinks';
+
+
+
+function App() {
+
+
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <SocialLinks />
+    </div>
+
+  )
+}
+
+export default App
