8b2f81c8caec8b6d3dda4e7479cd85596ddcf90d
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2023-12-12 14:42:45 +0000

    Portfolio updated

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,22 +1,20 @@
 import portfolio1 from "../assets/Movix.png";
-
-
+import portfolio5 from "../assets/pokedex.png";
+import portfolio6 from "../assets/hotstar.png";
 const Portfolio = () => {
   const portfolios = [
     { id: 1, src: portfolio1 },
     { id: 2, src: portfolio1 },
     { id: 3, src: portfolio1 },
     { id: 4, src: portfolio1 },
-    { id: 5, src: portfolio1 },
-    { id: 6, src: portfolio1 },
-
-
+    { id: 5, src: portfolio5 },
+    { id: 6, src: portfolio6 },
   ];
 
   return (
     <div
       name="portfolio"
-      className="bg-gradient-to-b from-purple-400 to-blue-200 w-full text-white mg:h-screen"
+      className="bg-gradient-to-b from-purple-400 to-blue-200 w-full text-white min-h-screen"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
@@ -26,34 +24,29 @@ const Portfolio = () => {
           <p className="py-6">Check out my work</p>
         </div>
 
-
-        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src }) => {
-            return (
-              <div className="shadow-md shadow-gray-500 rounded-lg " key={id}>
-                <div className="aspect-w-16 aspect-h-9 max-h-full">
-
-                  <img
-                    src={src}
-                    alt=""
-                    className="object-contain rounded-md duration-300 hover:scale-150 max-h-full"
-                  />
-                </div>
-                <div className="flex items-center justify-center">
-                  <button className="w-1/2 px-6 py-3 m-4 duration-150 hover:scale-105">
-                    Demo
-                  </button>
-                  <button className="w-1/2 px-6 py-3 m-4 duration-150 hover:scale-105">
-                    COde
-                  </button>
-                </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 text-center py-8 px-4 sm:px-0">
+          {portfolios.map(({ id, src }) => (
+            <div className="shadow-md rounded-lg" key={id}>
+              <div className="aspect-w-16 aspect-h-9 max-h-[11rem]">
+                <img
+                  src={src}
+                  alt=""
+                  className="object-cover w-full h-full rounded-md duration-300 hover:scale-150"
+                />
               </div>
-            );
-          })}
+              <div className="flex items-center justify-center mt-2">
+                <button className="w-1/2 px-6 py-3 m-2 duration-150 hover:scale-105 bg-blue-500 text-white rounded-md">
+                  Demo
+                </button>
+                <button className="w-1/2 px-6 py-3 m-2 duration-150 hover:scale-105 bg-green-500 text-white rounded-md">
+                  Code
+                </button>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
   );
 };
-
 export default Portfolio;
