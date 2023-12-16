a5104b84bfdd55ee68f420f8a164406eeb426ea4
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2023-12-16 15:45:30 +0000

    Update 2 files

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,14 +1,27 @@
 import portfolio1 from "../assets/Movix.png";
 import portfolio5 from "../assets/pokedex.png";
 import portfolio6 from "../assets/hotstar.png";
+import portfolio2 from "../assets/lms.jpg";
 const Portfolio = () => {
   const portfolios = [
     { id: 1, src: portfolio1 },
-    { id: 2, src: portfolio1 },
-    { id: 3, src: portfolio1 },
-    { id: 4, src: portfolio1 },
-    { id: 5, src: portfolio5 },
-    { id: 6, src: portfolio6 },
+    {
+      id: 2,
+      src: portfolio2,
+      code: "https://github.com/Harshksaw/LMS-Frontend",
+    },
+    {
+      id: 5,
+      src: portfolio5,
+      code: "https://github.com/Harshksaw/Pokedex-React",
+      demo: "https://harshksaw.github.io/",
+    },
+    {
+      id: 6,
+      src: portfolio6,
+      demo: "https://harshksaw.github.io/Hotstart_clone/",
+      code: "https://github.com/Harshksaw/Hotstart_clone",
+    },
   ];
 
   return (
@@ -24,22 +37,30 @@ const Portfolio = () => {
           <p className="py-6">Check out my work</p>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 text-center py-8 px-4 sm:px-0">
-          {portfolios.map(({ id, src }) => (
-            <div className="shadow-md rounded-lg" key={id}>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 text-center py-12 px-4 sm:px-0">
+          {portfolios.map(({ id, src, demo, code }) => (
+            <div className="shadow-md rounded-lg py-5 sm:mb-10" key={id}>
               <div className="aspect-w-16 aspect-h-9 max-h-[11rem]">
                 <img
                   src={src}
                   alt=""
-                  className="object-cover w-full h-full rounded-md duration-300 hover:scale-150"
+                  className="object-cover w-full h-full rounded-md duration-300 hover:scale-110 sm: max-h-[18rem]"
                 />
               </div>
-              <div className="flex items-center justify-center mt-2">
+              <div className="flex items-center justify-center mt-5">
                 <button className="w-1/2 px-6 py-3 m-2 duration-150 hover:scale-105 bg-blue-500 text-white rounded-md">
-                  Demo
+                  <a
+                    href={DOMImplementation}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Demo
+                  </a>
                 </button>
                 <button className="w-1/2 px-6 py-3 m-2 duration-150 hover:scale-105 bg-green-500 text-white rounded-md">
-                  Code
+                  <a href={code} target="_blank" rel="noopener noreferrer">
+                    Code
+                  </a>
                 </button>
               </div>
             </div>
