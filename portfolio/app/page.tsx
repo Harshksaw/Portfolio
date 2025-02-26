7cf3f8a231953d8ddbd190fd79ffa57ac281a97e
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2025-02-26 08:55:39 +0000

    next error

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -5,7 +5,7 @@ import { navItems } from "@/data";
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
 import Footer from "@/components/Footer";
-import Clients from "@/components/Clients";
+
 import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
