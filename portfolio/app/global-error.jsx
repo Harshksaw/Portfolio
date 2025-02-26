a88d79f73e7e4f64e4d76d75dc93054d7e752731
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2025-02-26 08:08:55 +0000

    remove sentry

--- portfolio/app/global-error.jsx
+++ portfolio/app/global-error.jsx
@@ -1,13 +1,11 @@
 "use client";
 
-import * as Sentry from "@sentry/nextjs";
+
 import Error from "next/error";
-import { useEffect } from "react";
+
 
 export default function GlobalError({ error }) {
-  useEffect(() => {
-    Sentry.captureException(error);
-  }, [error]);
+
 
   return (
     <html>
--- portfolio/next.config.mjs
+++ portfolio/next.config.mjs
@@ -1,4 +1,3 @@
-import { withSentryConfig } from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
@@ -8,14 +7,4 @@ const nextConfig = {
   },
 };
 
-export default withSentryConfig(nextConfig, {
-  silent: true,
-  org: "javascript-mastery",
-  project: "javascript-nextjs",
-}, {
-  widenClientFileUpload: true,
-  transpileClientSDK: true,
-  hideSourceMaps: true,
-  disableLogger: true,
-  automaticVercelMonitors: true,
-});
+export default nextConfig;
